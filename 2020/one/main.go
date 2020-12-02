5fae0a20daffc0b8db7470de7d07c7dbecb3e665
KerryAlsace <kerrydougherty@gmail.com>
2020-12-02 08:52:34 -0500

    part two answer

--- 2020/one/main.go
+++ 2020/one/main.go
@@ -10,8 +10,10 @@ import (
 func main() {
 	i := getInput("input.txt")
 	partOneAnswer := partOne(i)
+	partTwoAnswer := partTwo(i)
 
 	fmt.Println(partOneAnswer)
+	fmt.Println(partTwoAnswer)
 }
 
 func getInput(fileName string) []string {
@@ -37,6 +39,33 @@ func partOne(i []string) int {
 
 			if a+b == 2020 {
 				return a * b
+			}
+		}
+	}
+
+	return 0
+}
+
+func partTwo(i []string) int {
+	for _, x := range i {
+		a, err := strconv.Atoi(x)
+		if err != nil {
+			panic("invalid input")
+		}
+		for _, y := range i {
+			b, err := strconv.Atoi(y)
+			if err != nil {
+				panic("invalid input")
+			}
+			for _, z := range i {
+				c, err := strconv.Atoi(z)
+				if err != nil {
+					panic("invalid input")
+				}
+
+				if a+b+c == 2020 {
+					return a * b * c
+				}
 			}
 		}
 	}
