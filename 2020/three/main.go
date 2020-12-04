2af8c9d9c037fe75aff5a14e6036c6978b8cde13
KerryAlsace <kerrydougherty@gmail.com>
2020-12-04 10:17:57 -0500

    attempts at part 1

--- 2020/three/main.go
+++ 2020/three/main.go
@@ -6,13 +6,18 @@ import (
 	"strings"
 )
 
+// sample_input ans part 1 is 7; part 2 is
+// sample_input_two ans part 1 is 1;
+
+// guessed 95, guessed 72
+
 func main() {
-	i := getInput("sample_input.txt")
+	i := getInput("input.txt")
 	partOneAnswer := partOne(i)
-	partTwoAnswer := partTwo(i)
+	// partTwoAnswer := partTwo(i)
 
 	fmt.Println(partOneAnswer)
-	fmt.Println(partTwoAnswer)
+	// fmt.Println(partTwoAnswer)
 }
 
 func getInput(fileName string) []string {
@@ -25,9 +30,27 @@ func getInput(fileName string) []string {
 }
 
 func partOne(i []string) int {
-	return 0
+	spacesRight := 0
+	totalTrees := 0
+
+	for _, row := range i {
+		if len(row) <= spacesRight {
+			spacesRight = spacesRight - (len(row) - 2)
+			continue
+		}
+
+		x := row[spacesRight]
+
+		if string(x) == "#" {
+			totalTrees++
+		}
+
+		spacesRight += 3
+	}
+
+	return totalTrees
 }
 
-func partTwo(i []string) int {
-	return 0
-}
+// func partTwo(i []string) int {
+// 	return 0
+// }
