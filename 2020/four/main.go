3a1d4e9a25329d23bcb2f30bb9007b8843aef7c4
KerryAlsace <kerrydougherty@gmail.com>
2020-12-05 13:29:09 -0500

    save

--- 2020/four/main.go
+++ 2020/four/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	i := getInput("sample_input.txt")
-	partOneAnswer := partOne(i)
-	partTwoAnswer := partTwo(i)
+	partOneAnswer := partOne(formatInput(i))
+	// partTwoAnswer := partTwo(i)
 
 	fmt.Println(partOneAnswer)
-	fmt.Println(partTwoAnswer)
+	// fmt.Println(partTwoAnswer)
 }
 
 func getInput(fileName string) []string {
@@ -24,10 +24,39 @@ func getInput(fileName string) []string {
 	return strings.Split(string(b), "\n")
 }
 
+func formatInput(i []string) []string {
+	var newInput []string
+	currentIndex := 0
+	lastLineOfLastInput := 0
+	for index, line := range i {
+		fmt.Println("here's the line")
+		fmt.Println(line)
+		if line == "" {
+			fmt.Println("line == ''")
+			fmt.Printf("index is %v line is %s\n", index, line)
+			lastLineOfLastInput = index - 1
+
+			var sb strings.Builder
+			for n := 0; n <= lastLineOfLastInput-currentIndex; n++ {
+				fmt.Printf("current index %v\n", currentIndex)
+				sb.WriteString(i[currentIndex])
+				currentIndex++
+			}
+
+			newInput = append(newInput, sb.String())
+		}
+	}
+
+	return newInput
+}
+
 func partOne(i []string) int {
+	// for _, r := range i {
+	// 	// fmt.Println(r)
+	// }
 	return 0
 }
 
-func partTwo(i []string) int {
-	return 0
-}
+// func partTwo(i []string) int {
+// 	return 0
+// }
