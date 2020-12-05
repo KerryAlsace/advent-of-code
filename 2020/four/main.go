89aec608db52ba45d785148a2afa4c01d15e5d82
KerryAlsace <kerrydougherty@gmail.com>
2020-12-05 14:57:14 -0500

    part one done

--- 2020/four/main.go
+++ 2020/four/main.go
@@ -6,13 +6,15 @@ import (
 	"strings"
 )
 
-func main() {
-	i := getInput("sample_input.txt")
-	partOneAnswer := partOne(formatInput(i))
-	// partTwoAnswer := partTwo(i)
+// sample input ans part 1 is 2;
 
-	fmt.Println(partOneAnswer)
-	// fmt.Println(partTwoAnswer)
+func main() {
+	i := getInput("input.txt")
+	// partOneAnswer := partOne(formatInput(i))
+	partTwoAnswer := partTwo(formatInput(i))
+
+	// fmt.Println(partOneAnswer)
+	fmt.Println(partTwoAnswer)
 }
 
 func getInput(fileName string) []string {
@@ -29,7 +31,6 @@ func formatInput(i []string) []string {
 	firstLineOfLastInput := 0
 	lastLineOfLastInput := 0
 	for index, line := range i {
-		fmt.Printf("index [%v] line [%s]\n", index, line)
 		if line == "" || index == len(i)-1 {
 			if line == "" {
 				lastLineOfLastInput = index - 1
@@ -37,13 +38,10 @@ func formatInput(i []string) []string {
 				lastLineOfLastInput = index
 			}
 
-			fmt.Printf("lastLineOfLastInput [%v]\n", lastLineOfLastInput)
-
 			var sb strings.Builder
 			for n := 0; n <= lastLineOfLastInput-firstLineOfLastInput; n++ {
-				fmt.Printf("n = [%v] lastlineof [%v], firstline of [%v] lastLineOfLastInput-firstLineOfLastInput = [%v]\n", n, lastLineOfLastInput, firstLineOfLastInput, lastLineOfLastInput-firstLineOfLastInput)
 				sb.WriteString(i[firstLineOfLastInput+n])
-				sb.WriteString("")
+				sb.WriteString(" ")
 			}
 
 			firstLineOfLastInput = index + 1
@@ -55,10 +53,27 @@ func formatInput(i []string) []string {
 }
 
 func partOne(i []string) int {
+	validPassports := 0
+
 	for _, r := range i {
-		fmt.Println(r)
+		comp := make(map[string]int)
+
+		n := strings.Split(r, " ")
+		for _, l := range n {
+			p := strings.Split(l, ":")
+			comp[p[0]] = 1
+		}
+
+		if isValid(comp) {
+			validPassports++
+		}
 	}
-	return 0
+
+	return validPassports
+}
+
+func isValid(i map[string]int) bool {
+	return i["ecl"] == 1 && i["pid"] == 1 && i["eyr"] == 1 && i["hcl"] == 1 && i["byr"] == 1 && i["iyr"] == 1 && i["hgt"] == 1
 }
 
 // func partTwo(i []string) int {
